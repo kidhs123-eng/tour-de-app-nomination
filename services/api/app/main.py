5936968bdb671e68a5f6bd62aa7e532f6cc5fbd2
kidhs123-eng <kidhs123@gmail.com>
2026-09-18 21:28:39 +0200

    Caddy fix (#1)

--- services/api/app/main.py
+++ services/api/app/main.py
@@ -4,20 +4,16 @@ from fastapi.responses import FileResponse
 
 app = FastAPI(title="Academy API")
 
-# Автоматический поиск файла index.html
-BASE_DIR = Path(__file__).resolve().parent  # Папка app/
 
-# 1. Путь для Docker (когда папка frontend скопирована в /app/frontend)
-HTML_PATH = BASE_DIR.parent / "frontend" / "index.html"
+BASE_DIR = Path(__file__).resolve().parent
 
-# 2. Резервный путь для локального запуска из PyCharm
-if not HTML_PATH.exists():
-    HTML_PATH = BASE_DIR.parent.parent.parent / "frontend" / "index.html"
+
+INDEX_PATH = BASE_DIR / "frontend" / "templates" / "index.html"
 
 
 @app.get("/")
 def read_root():
-    return FileResponse(HTML_PATH)
+    return FileResponse(INDEX_PATH)
 
 
 @app.get("/api/health")
